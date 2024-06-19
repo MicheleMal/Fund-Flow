1d3428819d8294d6cca3dd86734ee56478851717
Michel <michelemalignaggi@gmail.com>
2024-06-19 18:08:58 +0200

    Delete the user, delete all associated information in other tables

--- backend/src/users/service/users.service.ts
+++ backend/src/users/service/users.service.ts
@@ -6,12 +6,24 @@ import { Model } from 'mongoose';
 import { decryptEmail, encryptEmail } from 'src/utils/crypto.util';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
+import { Earning } from 'src/schemas/Earning.schema';
+import { EarningSource } from 'src/schemas/EarningSource.schema';
+import { Expense } from 'src/schemas/Expense.schema';
+import { ExpenseSource } from 'src/schemas/ExpenseSource.schema';
+import { TotalEarnings } from 'src/schemas/TotalEarnings.schema';
+import { TotalExpenses } from 'src/schemas/TotalExpenses.schema';
 
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
+    @InjectModel(Earning.name) private readonly earningModel: Model<Earning>,
+    @InjectModel(EarningSource.name) private readonly earningSourceModel: Model<EarningSource>,
+    @InjectModel(Expense.name) private readonly expenseModel: Model<Expense>,
+    @InjectModel(ExpenseSource.name) private readonly expenseSourceModel: Model<ExpenseSource>,
+    @InjectModel(TotalEarnings.name) private readonly totalEarningModel: Model<TotalEarnings>,
+    @InjectModel(TotalExpenses.name) private readonly totalExpenseModel: Model<TotalExpenses>,
   ) {}
 
   async getInformationUser(request: Request): Promise<UserDto> {
@@ -49,6 +61,32 @@ export class UsersService {
     const _id = request["user"]._id
 
     const deleteUser = await this.userModel.findOneAndDelete({_id: _id}).exec()
+
+    if(deleteUser){
+      await this.earningModel.deleteMany({
+        id_user: _id
+      }).exec()
+
+      await this.earningSourceModel.deleteMany({
+        id_user: _id
+      }).exec()
+
+      await this.expenseModel.deleteMany({
+        id_user: _id
+      }).exec()
+
+      await this.expenseSourceModel.deleteMany({
+        id_user: _id
+      }).exec()
+      
+      await this.totalEarningModel.deleteMany({
+        id_user: _id
+      }).exec()
+      
+      await this.totalExpenseModel.deleteMany({
+        id_user: _id
+      }).exec()
+    }
 
     return deleteUser
   }
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -6,6 +6,12 @@ import { User, UserSchema } from 'src/schemas/User.schema';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { Earning, EarningSchema } from 'src/schemas/Earning.schema';
+import { EarningSource, EarningSourceSchema } from 'src/schemas/EarningSource.schema';
+import { Expense, ExpenseSchema } from 'src/schemas/Expense.schema';
+import { ExpenseSource, ExpenseSourceSchema } from 'src/schemas/ExpenseSource.schema';
+import { TotalEarnings, TotalEarningsSchema } from 'src/schemas/TotalEarnings.schema';
+import { TotalExpenses, TotalExpensesSchema } from 'src/schemas/TotalExpenses.schema';
 
 @Module({
   imports: [
@@ -13,7 +19,31 @@ import { AuthModule } from './auth/auth.module';
       {
         name: User.name,
         schema: UserSchema
-      }
+      },
+      {
+        name: Earning.name,
+        schema: EarningSchema
+      },
+      {
+        name: EarningSource.name,
+        schema: EarningSourceSchema
+      },
+      {
+        name: Expense.name,
+        schema: ExpenseSchema
+      },
+      {
+        name: ExpenseSource.name,
+        schema: ExpenseSourceSchema
+      },
+      {
+        name: TotalEarnings.name,
+        schema: TotalEarningsSchema
+      },
+      {
+        name: TotalExpenses.name,
+        schema: TotalExpensesSchema
+      },
     ]),
     AuthModule
   ],
